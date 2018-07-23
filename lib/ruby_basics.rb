2309d4083ad6d1e9e34c8fd696470b9a8a1cabb2
Sean Logan Para <spara77@gmail.com>
2018-07-23 19:17:16 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,25 +3,25 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  value = "value"
-end
-
-def argue
   
 end
 
-def greeting
+def argue
+  return argue
+end
 
+def greeting(greeting,name)
+  "#{greeting} #{name}"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(thing = "cheese")
+  return thing
 end
